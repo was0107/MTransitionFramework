107a689c8874363cb6b40cbceb30a8a53a33231a
micker <goodluck.was@gmail.com>
2017-05-16 17:00:29 +0800

    update

--- MTransitionFramework/MTransitionFramework/MTransitionFramework.h
+++ MTransitionFramework/MTransitionFramework/MTransitionFramework.h
@@ -16,7 +16,7 @@ FOUNDATION_EXPORT const unsigned char MTransitionFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MTransitionFramework/PublicHeader.h>
 
-#import "UINavigationController+MNavigationTransitioningLeftPush.h"
 #import "MNavigationTransitioning.h"
 #import "MNavigationTransitioningLeftPush.h"
 #import "MNavigationControllerDelegate.h"
+#import "UINavigationController+Transitioning.h"
